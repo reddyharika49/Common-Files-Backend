0503531d4f9e66ba9a4bf2bdb278b4387e5f9b44
akill1 <akhiljustifier82@gmail.com>
2026-02-12 17:27:42 +0530

    by Akhil logs error solved

--- src/main/java/com/common/entity/EmployeeView.java
+++ src/main/java/com/common/entity/EmployeeView.java
@@ -1,7 +1,7 @@
 package com.common.entity;
- 
+
 import org.hibernate.annotations.Immutable;
- 
+
 import jakarta.persistence.AttributeOverride;
 import jakarta.persistence.AttributeOverrides;
 import jakarta.persistence.Column;
@@ -9,43 +9,42 @@ import jakarta.persistence.EmbeddedId;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Table;
 import lombok.Data;
- 
+
 @Entity
 @Data
 @Immutable
-@Table(name = "sce_user_admin")
+@Table(name = "sce_user_admin", schema = "sce_admin")
 public class EmployeeView {
- 
+
     @EmbeddedId
     @AttributeOverrides({
-        @AttributeOverride(name = "empId", column = @Column(name = "emp_id")),
-        @AttributeOverride(name = "userRoleId", column = @Column(name = "user_role_id"))
+            @AttributeOverride(name = "empId", column = @Column(name = "emp_id")),
+            @AttributeOverride(name = "userRoleId", column = @Column(name = "user_role_id"))
     })
     private EmployeeViewId id;
- 
+
     @Column(name = "first_name")
     private String firstName;
- 
+
     @Column(name = "last_name")
     private String lastName;
- 
+
     @Column(name = "password")
     private String password;
- 
+
     @Column(name = "email")
     private String email;
- 
+
     @Column(name = "designation_name")
     private String designationName;
- 
+
     @Column(name = "role_name")
     private String roleName;
- 
+
     // REMOVED screenName and permissionName fields
- 
+
     // NEW field to map the 'screen_permission' JSON column
     // CHANGE THIS BACK TO STRING
     @Column(name = "screen_permission", columnDefinition = "json")
     private String screenPermission;
 }
- 
