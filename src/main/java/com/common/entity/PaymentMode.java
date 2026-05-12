bafdf94dee487745a02e88573c761739badcc15f
Tejaveerandra <patibandlatejaveerandra1234@gmail.com>
2026-05-12 16:22:35 +0530

    Update PaymentMode entity mapping to sce_pay_mode and add audit columns

--- src/main/java/com/common/entity/PaymentMode.java
+++ src/main/java/com/common/entity/PaymentMode.java
@@ -1,6 +1,11 @@
 package com.common.entity;
 
+import java.time.LocalDateTime;
+
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
@@ -11,12 +16,29 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 @Entity
-@Table(name = "sce_payment_mode" , schema = "sce_student")
+@Table(name = "sce_pay_mode", schema = "sce_stud_payments")
 public class PaymentMode {
-	
+
 	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "pay_mode_id")
 	private int payment_mode_id;
+
+	@Column(name = "pay_mode_name")
 	private String payment_type;
-//	private int is_active;
+
+	@Column(name = "is_active")
+	private int is_active;
+
+	@Column(name = "created_by")
 	private int created_by;
+
+	@Column(name = "updated_by")
+	private Integer updated_by;
+
+	@Column(name = "created_date")
+	private LocalDateTime created_date;
+
+	@Column(name = "updated_date")
+	private LocalDateTime updated_date;
 }
